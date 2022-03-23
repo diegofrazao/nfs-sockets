66bd63a3cbc0ed27ce2c8ef24674b490a4f6e681
Diego Frazão <diiegofrazao10@gmail.com>
2022-03-22 23:14:53 -0300

    feat: implementada atividade nfs-socket

--- src/br/edu/ifpb/gugawag/so/sockets/Client.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Client.java
@@ -1,18 +1,30 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 7000);
-        Scanner in = new Scanner(socket.getInputStream());
-        while(in.hasNextLine()) {
-            System.out.println(in.nextLine());
+
+        System.out.println("\n###   Comandos do Cliente    ###\n" +
+                            "# reddir {CAMINHO DA PASTA}    #\n" +
+                            "# rename {ARQUIVO} {NOVO NOME} #\n" +
+                            "# create {ARQUIVO}             #\n" +
+                            "# remove {ARQUIVO}             #\n");
+
+        Socket socket = new Socket("127.0.0.1", 7001);
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        while (true) {
+            System.out.print("~ ");
+            Scanner teclado = new Scanner(System.in);
+            dos.writeUTF(teclado.nextLine());
+
+            System.out.println(dis.readUTF());
         }
-        in.close();
-        socket.close();
     }
 }
--- src/br/edu/ifpb/gugawag/so/sockets/Server.java
+++ src/br/edu/ifpb/gugawag/so/sockets/Server.java
@@ -1,23 +1,74 @@
 package br.edu.ifpb.gugawag.so.sockets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(7000);
-        System.out.println("==Servidor==");
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente " + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+        System.out.println("== Servidor ==");
 
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-            out.println("Tchau!");
-            out.close();
-            clientSocket.close();
+        ServerSocket serverSocket = new ServerSocket(7001);
+        Socket socket = serverSocket.accept();
+
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        while (true) {
+            System.out.println("Cliente: " + socket.getInetAddress());
+
+            String mensagem = dis.readUTF();
+
+            String[] list = mensagem.split(" ");
+
+            switch (list[0]) {
+                case "reddir": {
+                    String resposta = "";
+                    File path = new File(list[1]);
+                    File[] arquivos = path.listFiles();
+
+                    if (arquivos != null) {
+                        for (File arquivo : arquivos) {
+                            resposta += arquivo.getName() + " ";
+                        }
+                        dos.writeUTF(resposta);
+                    }
+                    break;
+                }
+                case "rename":{
+                    File arquivo = new File(list[1]);
+                    File novoNome = new File(list[2]);
+
+                    if (arquivo.renameTo(novoNome))
+                        dos.writeUTF("Arquivo renomeado: " + arquivo.getName());
+                    else
+                        dos.writeUTF("Arquivo não foi renomeado!");
+                    break;
+                }
+                case "create": {
+                    File arquivo = new File(list[1]);
+
+                    if (arquivo.createNewFile())
+                        dos.writeUTF("Arquivo criado: " + arquivo.getName());
+                    else
+                        dos.writeUTF("Arquivo não foi criado!");
+                    break;
+                }
+                case "remove": {
+                    File arquivo = new File(list[1]);
+
+                    if (arquivo.delete())
+                        dos.writeUTF("Arquivo removido" + arquivo.getName());
+                    else
+                        dos.writeUTF("Arquivo não foi removido!");
+                    break;
+                }
+                default: {
+                    System.out.println("Opção inválida!");
+                    break;
+                }
+            }
         }
-
     }
 }
